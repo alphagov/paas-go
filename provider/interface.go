2047e7a0afefcb2a01fe12e3d622a11fb39170bd
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-10-02 09:23:48 +0100

    Add importable broker library
    This implements the brokerapi library's service broker interface and
    creates a new interface for all operations to be carried out by the
    'provider'. This is because most of the broker implementation is the
    same across brokers, so is basically boilerplate.

---
+++ provider/interface.go
@@ -0,0 +1,17 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/pivotal-cf/brokerapi"
+)
+
+//go:generate counterfeiter -o fakes/fake_service_provider.go . ServiceProvider
+type ServiceProvider interface {
+	Provision(context.Context, ProvisionData) (dashboardURL, operationData string, err error)
+	Deprovision(context.Context, DeprovisionData) (operationData string, err error)
+	Bind(context.Context, BindData) (binding brokerapi.Binding, err error)
+	Unbind(context.Context, UnbindData) (err error)
+	Update(context.Context, UpdateData) (operationData string, err error)
+	LastOperation(context.Context, LastOperationData) (state brokerapi.LastOperationState, description string, err error)
+}
